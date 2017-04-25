2ddfb7846191189ea05a796d0654f8c1395f10d2
shwu <brandon.shwu@gmail.com>
2017-04-25 17:23:05 +0800

    Add dist directory.

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const errorHandler = require('./middleware/error-handler.js');
 
 const app = express();
 
-app.use(requestLogger);
+// app.use(requestLogger);
 app.use(express.static('dist'));
 app.use('/api', postRouter);
 app.use(errorHandler);
 
-const port = 80;
+const port = 8080;
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}...`);
 });
